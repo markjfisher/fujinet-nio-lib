4dcb5f0d4e665c49d9c97d7b1221bef64447155b
Mark Fisher <mark.j.fisher@gmail.com>
2026-07-29 19:54:37 +0100

    Add pillow to beebium tests to allow capturing png screenshots

--- integration-tests/beebium/run_pytest.sh
+++ integration-tests/beebium/run_pytest.sh
@@ -20,4 +20,4 @@ fi
 cd "$here"
 # Use python -m pytest so the editable beebium client is on sys.path (.venv/bin/pytest
 # alone does not pick up --with-editable).
-exec uv run --python "${BEEBIUM_PYTHON:-python3}" --with-editable "${client}" python -m pytest -p no:beebium "$@"
+exec uv run --python "${BEEBIUM_PYTHON:-python3}" --with Pillow --with-editable "${client}" python -m pytest -p no:beebium "$@"
